bf59ddbeccc6482710f4de55e41e129d9f695e9b
Georgi Georgiev <georgi.georgiev@docuware.com>
2020-05-24 23:44:43 +0300

    Default ASP.NET template created.

---
+++ FaceAnonymizer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FaceAnonymizer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
